7c0cab63d962ea39f656589e14ca6ace85a5345c
Valerie Kaiser <github@vakaiser.com>
2023-12-02 12:23:49 -0800

    Got a basic title up.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="flex flex-col items-center justify-center h-screen overflow-hidden bg-black text-white">
+  <!-- Title Animation-->
+  <div class=" flex items-center justify-center p-4 mx-4 bg-black border border-white rounded-lg">
+    <h1 class="text-5xl tracking-[.5rem] font-thin font-poppins select-none">VALERIE KAISER</h1>
+  </div>
+</main>
