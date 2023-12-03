66db5077294fa1d5eac8480665999a08cbe1cb70
Valerie Kaiser <github@vakaiser.com>
2023-12-03 00:02:02 -0800

    Animation tweaks.

--- src/lib/Visualizer.svelte
+++ src/lib/Visualizer.svelte
@@ -3,23 +3,23 @@
 
   let circles: HTMLDivElement;
   let colors = [
-    'bg-red-500',
-    'bg-orange-500',
-    'bg-amber-500',
-    'bg-yellow-500',
-    'bg-lime-500',
-    'bg-green-500',
-    'bg-emerald-500',
-    'bg-teal-500',
-    'bg-cyan-500',
-    'bg-sky-500',
-    'bg-blue-500',
-    'bg-indigo-500',
-    'bg-violet-500',
-    'bg-purple-500',
-    'bg-fuchsia-500',
-    'bg-pink-500',
-    'bg-rose-500'
+    'border-red-500',
+    'border-orange-500',
+    'border-amber-500',
+    'border-yellow-500',
+    'border-lime-500',
+    'border-green-500',
+    'border-emerald-500',
+    'border-teal-500',
+    'border-cyan-500',
+    'border-sky-500',
+    'border-blue-500',
+    'border-indigo-500',
+    'border-violet-500',
+    'border-purple-500',
+    'border-fuchsia-500',
+    'border-pink-500',
+    'border-rose-500'
   ];
 
   onMount(() => {
@@ -37,7 +37,7 @@
           { offset: 0, transform: 'translate(-50%, -50%) scale(0.0001)' },
           {
             offset: 1,
-            transform: `translate(-50%, -50%) scale(1.5) rotateZ(${
+            transform: `translate(-50%, -50%) scale(1.75) rotateZ(${
               (index + 1) * (90 / colors.length)
             }deg)`
           }
