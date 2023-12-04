a17cb84872e97d74990789dbe384bde951922390
Valerie Kaiser <github@vakaiser.com>
2023-12-03 19:18:49 -0800

    Tweaked build pipeline.

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ const config = {
   kit: {
     adapter: adapter({ fallback: 'index.html' }),
     paths: {
-      base: process.argv.includes('dev') ? '' : '/v-a-kaiser/vakaiser.com'
+      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
     }
   },
   preprocess: vitePreprocess()
