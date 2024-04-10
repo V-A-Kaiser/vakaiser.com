2b6e1ef9056cdb8fd949b33890c8561a76e7bbaa
Valerie Kaiser <github@vakaiser.com>
2024-04-09 19:17:42 -0700

    Hardcoded base path.

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ const config = {
   kit: {
     adapter: adapter({ fallback: 'index.html' }),
     paths: {
-      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+      base: ''
     }
   },
   preprocess: vitePreprocess()
