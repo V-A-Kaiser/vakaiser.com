f081561f985874cd7eafc9c07a03d5b8cd031925
Valerie Kaiser <github@vakaiser.com>
2024-07-01 21:54:55 -0700

    Updates to resume.

--- src/lib/Resume.svelte
+++ src/lib/Resume.svelte
@@ -1,10 +1,10 @@
-<div class="text-slate-800 bg-white h-[11in] w-[8.5in] z-10 flex flex-col shadow-lg">
+<div class="text-gray-900 bg-white h-[11in] w-[8.5in] z-10 flex flex-col shadow-lg">
   <!-- Title and Contact Information -->
-  <div class="text-center p-4 bg-gradient-to-b from-indigo-950 to-gray-900 text-slate-100 font-title">
-    <div class="text-4xl font-thin">VALERIE KAISER</div>
+  <div
+    class="text-center p-4 bg-gray-900 text-white font-title"
+  >
+    <div class="text-4xl font-thin italic tracking-wider">VALERIE <span class="font-bold">KAISER</span></div>
     <div class="flex justify-center gap-4 text-sm mt-2">
-      <div>206 - 650 - 5259</div>
-      <div>✦</div>
       <div>resume@vakaiser.com</div>
       <div>✦</div>
       <div>linkedin.com/in/vakaiser</div>
@@ -17,44 +17,47 @@
 
     <!-- Cart.com -->
     <div class="mb-4">
-      <div class="font-bold flex justify-between">
+      <div class="font-bold flex justify-between text-lg">
         <p>Cart.com</p>
         <p>February 2021 &mdash; Present</p>
       </div>
 
-      <div class="italic flex justify-between">
+      <div class="italic flex justify-between my-2">
         <p>CloudOps Engineer</p>
         <p>January 2022 &mdash; Present</p>
       </div>
 
       <ul class="list-disc ms-8 mb-2">
         <li>
-          Designed and implemented an IaC solution for managing the shared dependencies of 15+ (and
-          growing) Kubernetes clusters in GCP and AWS via Helm, Terraform, and GitHub Actions.
+          Designed and implemented an declarative IaC solution for managing the shared dependencies
+          of 15+ Kubernetes clusters in GCP and AWS via Helm, Terraform, and GitHub Actions.
         </li>
         <li>
-          Refactored the deployment process for microservices across the organization via
-          centralized Helm charts, abstracted GitHub Action workflows, and Terraform.
+          Refactored the Kubernetes deployment process for microservices across the organization via
+          centralized Helm charts, reusable GitHub Action workflows, and Terraform.
         </li>
         <li>
-          Served as an evangelist for Cart's CloudOps practice through cross team collaboration and
-          problem solving.
+          Worked with numerous teams across various time zones to solve issues with
+          networking, code deployment, environment management, cloud infrastructure, and
+          Kubernetes.
         </li>
         <li>
-          Migrated repositories, pipelines, and teams over to GitHub from GitLab and BitBucket.
+          Worked alongside the SecOps team to implement security tooling and address leaks and security breaches.
         </li>
-        <li>Created a Slack bot to announce pipeline updates.</li>
+        <li>
+          Served as an evangelist for the CloudOps team and led weekly "Lunch and Learn" knowledge transfer sessions.
+        </li>
       </ul>
 
-      <div class="italic flex justify-between">
+      <div class="italic flex justify-between mb-2">
         <p>Frontend Engineer</p>
         <p>February 2021 &mdash; January 2022</p>
       </div>
 
       <ul class="list-disc ms-8">
         <li>
-          Built the first 3 iterations of the Cart.com Brand Console front-end using React.js,
-          Typescript, and GraphQL.
+          Built the first 3 iterations of the Cart.com Brand Console front-end using Auth0, React.js, Tailwind, GraphQL, and
+          Typescript.
         </li>
         <li>
           Established CI/CD pipelines and a robust deployment process for the Brand Console via
@@ -65,17 +68,20 @@
 
     <!-- AGS -->
     <div class="mb-4">
-      <div class="font-bold flex justify-between">
+      <div class="font-bold flex justify-between text-lg">
         <p>AGS, LLC</p>
         <p>January 2018 &mdash; February 2021</p>
       </div>
 
-      <div class="italic">
+      <div class="italic my-2">
         <p>Software Engineer</p>
       </div>
 
       <ul class="list-disc ms-8">
-        <li>Addressed a litany of DevOps issues involving SVN, custom tool chains, and proprietary hardware.</li>
+        <li>
+          Addressed a litany of DevOps issues involving SVN, custom tool chains, and proprietary
+          hardware.
+        </li>
         <li>Sole software engineer for the 10 video slot games below:</li>
       </ul>
 
@@ -92,26 +98,6 @@
         <div>Lightning Wheel</div>
       </div>
     </div>
-
-    <!-- NASA Subcontract -->
-    <div>
-      <div class="font-bold flex justify-between">
-        <p>NASA</p>
-        <p>May 2017 &mdash; January 2018</p>
-      </div>
-
-      <div class="italic">
-        <p>Software Engineer (Contract)</p>
-      </div>
-
-      <ul class="list-disc ms-8">
-        <li>
-          Small miscellaneous open source contributions to NASA's Trick Simulation Environment.
-          <br />
-          (https://github.com/nasa/trick)
-        </li>
-      </ul>
-    </div>
   </div>
 
   <!-- Education -->
@@ -122,12 +108,8 @@
       <p>Beaumont, TX</p>
     </div>
     <div class="flex justify-between">
-      <p>B.S. Computer Science</p>
+      <p>B.S. Computer Science (Cum Laude)</p>
       <p>August 2013 &mdash; December 2016</p>
-    </div>
-    <div class="flex justify-between">
-      <p>Cum Laude</p>
-      <p>3.66 GPA</p>
     </div>
   </div>
 
